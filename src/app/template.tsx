3b4dd419848793ab5ecc238afc8d9b8681a8ebd7
Anyul Rivas <Anyul.Rivas@ingrammicro.com>
2024-09-11 16:41:35 +0200

    feat: page transitions

---
+++ src/app/template.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+/*
+       initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+* */
+
+export default function Template({ children }: { children: React.ReactNode }) {
+    return (
+        <motion.div
+            initial={{ y: 20, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ ease: "easeInOut", duration: 0.75 }}
+        >
+            {children}
+        </motion.div>
+    );
+}
