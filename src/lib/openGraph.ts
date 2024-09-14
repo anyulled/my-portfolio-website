a1d7888e5ac251c8b799166a2ff95a0e47ec8dc9
Anyul Rivas <Anyul.Rivas@ingrammicro.com>
2024-09-14 13:34:40 +0200

    feat: open graph images

--- src/lib/openGraph.ts
+++ src/lib/openGraph.ts
@@ -1,17 +1,16 @@
 export const openGraph = {
-    title: "Sensuelle Boudoir",
-    description: "Boudoir photography service in Barcelona.",
-    url: "https://boudoir.barcelona",
-    siteName: "Boudoir photography service in Barcelona",
-    images: [
-        {
-            url:
-                "https://live.staticflickr.com/65535/53367295647_2ff0fdf881_h.jpg",
-            width: 1200,
-            height: 630,
-            alt: "Boudoir photography in Barcelona",
-        },
-    ],
-    locale: "en_US",
-    type: "website",
+  title: "Sensuelle Boudoir",
+  description: "Boudoir photography service in Barcelona.",
+  url: "https://boudoir.barcelona",
+  siteName: "Boudoir photography service in Barcelona",
+  images: [
+    {
+      url: "https://live.staticflickr.com/65535/53367295647_2ff0fdf881_h.jpg",
+      width: 1200,
+      height: 630,
+      alt: "Boudoir photography in Barcelona",
+    },
+  ],
+  locale: "en_US",
+  type: "website",
 };
