883906f02eba41bb964148227608c79d282294e0
Anyul Rivas <Anyul.Rivas@ingrammicro.com>
2024-09-16 18:55:40 +0200

    feat: open graph images v.4

--- src/app/styles/[styleName]/opengraph-image.tsx
+++ src/app/styles/[styleName]/opengraph-image.tsx
@@ -1,13 +1,8 @@
 import { ImageResponse } from "next/og";
 import { extractNameFromTag } from "@/lib/extractName";
-import { Dancing_Script } from "next/font/google";
-
 import stylesData from "@/data/styles.json";
-import { getFlickrPhotos } from "@/services/flickr";
-
-const dancingScript = Dancing_Script({ subsets: ["latin"] });
-
-export const runtime = "edge";
+import { getFlickrPhotos } from "@/services/flickr"; /*eslint-disable @next/next/no-img-element */
+/*eslint-disable @next/next/no-img-element */
 
 export default async function OpengraphImage({
   params,
@@ -19,28 +14,24 @@ export default async function OpengraphImage({
   const result = await getFlickrPhotos(convertedStyleName, "1");
   return new ImageResponse(
     (
-      <div
-        style={{
-          fontSize: 128,
-          background: "black",
-          backgroundImage: ` url(${result.photos?.at(0)?.urlNormal})`,
-          backgroundPosition: "center",
-          width: "100%",
-          flexDirection: "column",
-          height: "100%",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <h1
-          className={`${dancingScript.className} text-5xl text-white pt-12 mb-2 mt-0 drop-shadow-2xl`}
-        >
-          Sensuelle Boudoir
-        </h1>
-        <h2 className={"text-3xl text-neutral-500 m-0 drop-shadow-2xl"}>
-          {styleName}
-        </h2>
+      <div tw="flex flex-col w-full h-full items-center justify-center bg-black">
+        <div tw="bg-neutral-800 flex flex-row w-full h-full">
+          <div tw="w-2/5 py-12 px-4 p-8 flex ">
+            <img
+              src={result.photos?.at(0)?.urlZoom}
+              alt={styleName}
+              tw="w-full h-auto rounded-md"
+            />
+          </div>
+          <div tw="mt-8 flex flex-col justify-center align-center">
+            <h1 tw="flex items-center justify-center rounded-md border border-transparent ml-20 px-5 pt-3 text-5xl font-medium text-white">
+              Sensuelle Boudoir
+            </h1>
+            <h2 tw="flex items-center justify-center rounded-md border border-transparent ml-20 px-5 py-1 text-3xl font-medium text-neutral-200">
+              {styleName}
+            </h2>
+          </div>
+        </div>
       </div>
     ),
   );
