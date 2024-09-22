1ae9f5d825995f8be94f70d17371fe5234c679a1
Anyul Rivas <Anyul.Rivas@ingrammicro.com>
2024-09-22 20:44:03 +0200

    feat: image galleries with react album

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,18 +1,20 @@
 "use client";
-import Image from "next/image";
+
+import "yet-another-react-lightbox/styles.css";
+import "yet-another-react-lightbox/plugins/captions.css";
+import "react-photo-album/rows.css";
 import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
 import Zoom from "yet-another-react-lightbox/plugins/zoom";
 import Captions from "yet-another-react-lightbox/plugins/captions";
-import "yet-another-react-lightbox/styles.css";
-import "yet-another-react-lightbox/plugins/captions.css";
-import { Aref_Ruqaa, Dancing_Script } from "next/font/google";
+import { Aref_Ruqaa } from "next/font/google";
 import { Photo } from "@/services/flickr";
-import useAnalyticsEventTracker from "@/hooks/eventTracker";
 import { useState } from "react";
 import Lightbox from "yet-another-react-lightbox";
+import { Image, RowsPhotoAlbum } from "react-photo-album";
+import useAnalyticsEventTracker from "@/hooks/eventTracker";
+import renderNextImage from "@/components/NextImage";
 
 const arefRuqaa = Aref_Ruqaa({ subsets: ["latin"], weight: "400" });
-const dancingScript = Dancing_Script({ subsets: ["latin"] });
 
 interface GalleryProps {
   photos: Array<Photo> | null;
@@ -25,13 +27,20 @@ export default function Gallery({
 }: Readonly<GalleryProps>) {
   const gaEventTracker = useAnalyticsEventTracker("Gallery");
 
-  const [photoIndex, setPhotoIndex] = useState<number>(0);
+  const [photoIndex, setPhotoIndex] = useState<number>(-1);
   const [lightboxOpen, setLightboxOpen] = useState<boolean>(false);
   const handleImageClick = (image: number) => {
     gaEventTracker("image_click", `Image ${image}`);
     setLightboxOpen(true);
     setPhotoIndex(image);
   };
+
+  const convertedPhotos: Image[] | undefined = photos?.map((photo) => ({
+    src: photo.urlZoom,
+    alt: photo.title,
+    width: parseInt(photo.width),
+    height: parseInt(photo.height),
+  }));
   return (
     <>
       <section className="py-6 md:py-3">
@@ -43,35 +52,13 @@ export default function Gallery({
               Our Gallery
             </h2>
           )}
-          {photos && (
-            <div className="columns-1 sm:columns-2 md:columns-3 lg:columns-4 gap-4 space-y-4">
-              {photos.map((image, i) => (
-                <div
-                  onClick={() => handleImageClick(i)}
-                  key={image.id}
-                  className="relative overflow-hidden group break-inside-avoid"
-                >
-                  <Image
-                    src={image.urlCrop}
-                    alt={image.title}
-                    width={600}
-                    height={400 + (i % 3) * 100}
-                    priority
-                    className="w-full h-auto transition-all duration-500 ease-in-out opacity-0 group-hover:opacity-100 group-hover:scale-105 rounded-lg"
-                    onLoad={(event) =>
-                      event.currentTarget.classList.remove("opacity-0")
-                    }
-                  />
-                  <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded-lg">
-                    <span
-                      className={`${dancingScript.className} text-white text-2xl`}
-                    >
-                      View More
-                    </span>
-                  </div>
-                </div>
-              ))}
-            </div>
+          {convertedPhotos && (
+            <RowsPhotoAlbum
+              render={{ image: renderNextImage }}
+              photos={convertedPhotos}
+              defaultContainerWidth={1200}
+              onClick={({ index }) => handleImageClick(index)}
+            />
           )}
         </div>
       </section>
