b0e1061e39cd85aa8fa98da27377763a7568bcd3
Anyul Rivas <anyul.rivas@ingrammicro.com>
2024-10-10 16:06:31 +0200

    feat: include random image functionality and text shadow

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,20 +1,51 @@
 "use client";
 import { Dancing_Script, Aref_Ruqaa } from "next/font/google";
 import { useScroll } from "@/contexts/ScrollContext";
+import React from "react";
+import { useTheme } from "next-themes";
 
 const arefRuqaa = Aref_Ruqaa({ subsets: ["latin"], weight: "400" });
 const dancingScript = Dancing_Script({ subsets: ["latin"] });
 
 export default function Hero() {
   const { scrollY } = useScroll();
+    const { theme } = useTheme();
+
+  const gradient = React.useMemo(() => {
+      if(theme === "dark"){
+          return "linear-gradient(to bottom, rgba(255,255,255,0.1) 75%, rgba(25,24,29,1) 98%)";
+      } else{
+          return "linear-gradient(to bottom, rgba(200,200,200,0.1) 82%, rgba(255,255,255,1) 98%)";
+      }
+  }, [theme]);
+  const randomImage = React.useMemo(() => {
+    const images = [
+      "https://live.staticflickr.com/65535/53564630658_b2aeab68f2_h.jpg",
+      "https://live.staticflickr.com/65535/53367295647_2ff0fdf881_h.jpg",
+      "https://live.staticflickr.com/65535/53923428207_dc41871f93_h.jpg",
+      "https://live.staticflickr.com/65535/53564630648_06aa8a167d_h.jpg",
+      "https://live.staticflickr.com/65535/53963952034_7372534fc0_h.jpg",
+    ];
+    return images[Math.floor(Math.random() * images.length)];
+  }, []);
 
   return (
     <section className="relative h-screen flex items-center justify-center overflow-hidden">
       <div
+        className="absolute"
+        style={{
+          background: gradient,
+          zIndex: 9,
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        &nbsp;
+      </div>
+      <div
         className="absolute inset-0 z-0"
         style={{
-          backgroundImage:
-            "url('https://live.staticflickr.com/65535/53367295647_2ff0fdf881_h.jpg')",
+          backgroundImage: `url('${randomImage}')`,
           backgroundAttachment: "fixed",
           backgroundPosition: "center",
           backgroundSize: "cover",
@@ -23,12 +54,12 @@ export default function Hero() {
       />
       <div className="relative z-10 text-center">
         <h1
-          className={`${dancingScript.className} text-5xl md:text-7xl font-bold mb-4 drop-shadow-2xl text-yellow-600 dark:text-white`}
+          className={`${dancingScript.className} text-5xl md:text-7xl font-bold mb-4 text-yellow-600 dark:text-white text-shadow-default shadow-neutral-800 dark:shadow-yellow-800`}
         >
           Sensuelle Boudoir
         </h1>
         <p
-          className={`${arefRuqaa.className} text-2xl md:text-3xl dark:text-neutral-400 text-yellow-700 drop-shadow-lg`}
+          className={`${arefRuqaa.className} text-2xl md:text-3xl text-yellow-600 dark:text-neutral-200 text-shadow-sm shadow-neutral-800 dark:shadow-yellow-800`}
         >
           Capture Your Essence
         </p>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
 
 const config: Config = {
   darkMode: ["class"],
@@ -58,8 +59,27 @@ const config: Config = {
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
+      textShadow: {
+        xs: "0 1px 1px var(--tw-shadow-color)",
+        sm: "0 1px 2px var(--tw-shadow-color)",
+        default: "0 2px 4px var(--tw-shadow-color)",
+        lg: "0 8px 16px var(--tw-shadow-color)",
+      },
     },
   },
-  plugins: [require("tailwindcss-animate"), require("tailwindcss-typography")],
+  plugins: [
+    require("tailwindcss-animate"),
+    require("tailwindcss-typography"),
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          "text-shadow": (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme("textShadow") },
+      );
+    }),
+  ],
 };
 export default config;
