60c205ae32b3a7c0476f3813df02ea034d47f10d
Anyul Rivas <anyul.rivas@ingrammicro.com>
2024-10-23 18:08:04 +0200

    chore: remove comment

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -3,12 +3,6 @@
 import { motion } from "framer-motion";
 import React from "react";
 
-/*
-       initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.8 }}
-* */
-
 export default function Template({
   children,
 }: Readonly<{
