b9b0deb559f2825e835643e400427ad569915cdd
Anyul Rivas <anyul.rivas@ingrammicro.com>
2024-11-14 10:22:35 +0100

    fix: reduce LCP by removing the cookie consent delay

--- src/components/CookieConsent.tsx
+++ src/components/CookieConsent.tsx
@@ -14,8 +14,7 @@ export default function CookieConsent() {
   useEffect(() => {
     const consent = localStorage.getItem("cookieConsent");
     if (!consent) {
-      const timer = setTimeout(() => setShowConsent(true), 2000);
-      return () => clearTimeout(timer);
+      setShowConsent(true);
     }
   }, []);
 
@@ -32,9 +31,7 @@ export default function CookieConsent() {
       className={`fixed bottom-0 left-0 right-0 p-2 ${theme === "dark" ? "bg-gray-800" : "bg-gray-100"} bg-opacity-75 backdrop-blur-sm z-50 text-sm transition-all duration-300 ${showConsent ? "translate-y-0" : "translate-y-full"}`}
     >
       <div className="container mx-auto flex justify-between items-center">
-        <p
-          className="dark:text-gray-300 text-neutral-800"
-        >
+        <p className="dark:text-gray-300 text-neutral-800">
           {t("cookie_message")}
         </p>
         <Button
