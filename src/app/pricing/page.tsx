66c27a49d86b73705180f2b0fbee3fb0964082a3
Anyul Rivas <anyul.rivas@ingrammicro.com>
2024-11-22 11:01:09 +0100

    feat: pricing page

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -1,9 +1,15 @@
-import { Dancing_Script, Aref_Ruqaa} from "next/font/google";
-import { Check } from "lucide-react";
+import { Dancing_Script, Aref_Ruqaa } from "next/font/google";
+import { Check, Camera, Clock, Shirt, Video } from "lucide-react";
 import { getTranslations } from "next-intl/server";
+import Image from "next/image";
+import { Metadata } from "next";
 
 const dancingScript = Dancing_Script({ subsets: ["latin"] });
 const arefRuqaa = Aref_Ruqaa({ subsets: ["latin"], weight: "400" });
+
+export const metadata: Metadata = {
+  title: " Pricing",
+};
 
 export default async function PricingPage() {
   const t = await getTranslations("pricing");
@@ -11,25 +17,62 @@ export default async function PricingPage() {
     {
       name: t("boudoir_experience"),
       price: "350 €",
+      image:
+        "https://live.staticflickr.com/65535/54154502487_981fb48243_c_d.jpg",
       features: [
-        `12 ${t("edited_photos")}`,
-        `4 ${t("clothing_change")}`,
-        `2 ${t("hours_studio")}`,
-        `200 ${t("photos_hd")}`,
-        t("clothing_and_attrezzo"),
+        {
+          icon: <Camera className="w-5 h-5" />,
+          text: `12 ${t("edited_photos")}`,
+        },
+        {
+          icon: <Shirt className="w-5 h-5" />,
+          text: `4 ${t("clothing_change")}`,
+        },
+        {
+          icon: <Clock className="w-5 h-5" />,
+          text: `2 ${t("hours_studio")}`,
+        },
+        {
+          icon: <Check className="w-5 h-5" />,
+          text: `200 ${t("photos_hd")}`,
+        },
+        {
+          icon: <Check className="w-5 h-5" />,
+          text: t("clothing_and_attrezzo"),
+        },
       ],
     },
     {
       name: t("deluxe_experience"),
       price: "600 €",
+      image:
+        "https://live.staticflickr.com/65535/53307099860_93b77dd6dc_k_d.jpg",
       features: [
-        `20 ${t("edited_photos")}`,
-        t("video"),
-        t("professional_makeup"),
-        `4 ${t("clothing_change")}`,
-        `3 ${t("hours_studio")}`,
-        `200 ${t("photos_hd")}`,
-        t("clothing_and_attrezzo"),
+        {
+          icon: <Camera className="w-5 h-5" />,
+          text: `20 ${t("edited_photos")}`,
+        },
+        { icon: <Video className="w-5 h-5" />, text: t("video") },
+        {
+          icon: <Check className="w-5 h-5" />,
+          text: t("professional_makeup"),
+        },
+        {
+          icon: <Shirt className="w-5 h-5" />,
+          text: `4 ${t("clothing_change")}`,
+        },
+        {
+          icon: <Clock className="w-5 h-5" />,
+          text: `3 ${t("hours_studio")}`,
+        },
+        {
+          icon: <Camera className="w-5 h-5" />,
+          text: `200 ${t("photos_hd")}`,
+        },
+        {
+          icon: <Check className="w-5 h-5" />,
+          text: t("clothing_and_attrezzo"),
+        },
       ],
     },
   ];
@@ -47,34 +90,46 @@ export default async function PricingPage() {
           {t("discover_our_experiences")}
         </p>
 
-        <div className="grid md:grid-cols-2 gap-8">
+        <div className="grid lg:grid-cols-2 gap-12">
           {packages.map((pkg, index) => (
             <div
               key={index}
-              className="dark:bg-neutral-800 bg-neutral-200 rounded-lg shadow-lg overflow-hidden"
+              className="dark:bg-neutral-800 bg-neutral-100 rounded-lg shadow-lg overflow-hidden flex flex-col md:flex-row"
             >
-              <div className="p-6 bg-gradient-to-r from-amber-700 to-amber-900">
-                <h2
-                  className={`${arefRuqaa.className} text-2xl md:text-3xl mb-2`}
-                >
-                  {pkg.name}
-                </h2>
-                <p
-                  className={`${dancingScript.className} text-4xl md:text-5xl`}
-                >
-                  {pkg.price}
-                </p>
+              <div className="md:w-2/5 relative">
+                <Image
+                  src={pkg.image}
+                  alt={pkg.name}
+                  width={300}
+                  height={400}
+                  className="object-cover w-full h-full"
+                />
+                <div className="absolute inset-0 bg-gradient-to-t from-neutral-900 to-transparent"></div>
+                <div className="absolute bottom-0 left-0 right-0 p-6">
+                  <h2
+                    className={`${arefRuqaa.className} text-2xl md:text-3xl mb-2 text-white`}
+                  >
+                    {pkg.name}
+                  </h2>
+                  <p
+                    className={`${dancingScript.className} text-4xl md:text-5xl text-white`}
+                  >
+                    {pkg.price}
+                  </p>
+                </div>
               </div>
-              <ul className="p-6 space-y-4">
-                {pkg.features.map((feature, featureIndex) => (
-                  <li key={featureIndex} className="flex items-start">
-                    <Check className="w-5 h-5 mr-2 text-amber-500 flex-shrink-0 mt-1" />
-                    <span>{feature}</span>
-                  </li>
-                ))}
-              </ul>
-              <div className="p-6 pt-0">
-                <button className="w-full bg-amber-600 hover:bg-amber-700 text-white font-bold py-2 px-4 rounded transition duration-300">
+              <div className="md:w-3/5 p-6 flex flex-col justify-between">
+                <ul className="space-y-4 mb-6">
+                  {pkg.features.map((feature, featureIndex) => (
+                    <li key={featureIndex} className="flex items-center">
+                      <span className="mr-2 text-peach-fuzz-500">
+                        {feature.icon}
+                      </span>
+                      <span>{feature.text}</span>
+                    </li>
+                  ))}
+                </ul>
+                <button className="w-full bg-peach-fuzz-600 hover:bg-peach-fuzz-700 text-white font-bold py-2 px-4 rounded transition duration-300">
                   {t("book_now")}
                 </button>
               </div>
