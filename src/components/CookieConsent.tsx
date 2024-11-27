26c27b2dce7c814802f5a5e29a63f22ef5942974
Anyul Rivas <anyul.rivas@ingrammicro.com>
2024-11-27 15:02:40 +0100

    chore: remove theme import

--- src/components/CookieConsent.tsx
+++ src/components/CookieConsent.tsx
@@ -28,7 +28,7 @@ export default function CookieConsent() {
 
   return (
     <div
-      className={`fixed bottom-0 left-0 right-0 p-2 ${theme === "dark" ? "bg-gray-800" : "bg-gray-100"} bg-opacity-75 backdrop-blur-sm z-50 text-sm transition-all duration-300 ${showConsent ? "translate-y-0" : "translate-y-full"}`}
+      className={`fixed bottom-0 left-0 right-0 p-2 dark:bg-gray-800 bg-gray-100 bg-opacity-75 backdrop-blur-sm z-50 text-sm transition-all duration-300 ${showConsent ? "translate-y-0" : "translate-y-full"}`}
     >
       <div className="container mx-auto flex justify-between items-center">
         <p className="dark:text-gray-300 text-neutral-800">
