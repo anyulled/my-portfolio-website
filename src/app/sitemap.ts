335952b71414e5408c91fd70daa6fcbf759dbee1
Anyul Rivas <anyul.rivas@ingrammicro.com>
2025-01-07 14:36:42 +0100

    feat: sitemap.ts

---
+++ src/app/sitemap.ts
@@ -0,0 +1,61 @@
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseURL = "https://boudoir.barcelona";
+  return [
+    {
+      url: baseURL,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: `${baseURL}/about`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: `${baseURL}/pricing`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
+    {
+      url: `${baseURL}/what-is-boudoir`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+    {
+      url: `${baseURL}/models`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.4,
+    },
+    {
+      url: `${baseURL}/styles`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.4,
+    },
+    {
+      url: `${baseURL}/legal`,
+      lastModified: new Date(),
+      changeFrequency: "never",
+      priority: 0.3,
+    },
+    {
+      url: `${baseURL}/cookies`,
+      lastModified: new Date(),
+      changeFrequency: "never",
+      priority: 0.3,
+    },
+    {
+      url: `${baseURL}/privacy`,
+      lastModified: new Date(),
+      changeFrequency: "never",
+      priority: 0.3,
+    },
+  ];
+}
