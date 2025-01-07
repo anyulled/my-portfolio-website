cb671a6bd95433f549fea18cd6752a9f51fe1b89
Anyul Rivas <anyul.rivas@ingrammicro.com>
2025-01-07 15:10:23 +0100

    feat: openTelemetry Observability

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,4 +1,7 @@
 import * as Sentry from "@sentry/nextjs";
+import { registerOTel } from "@vercel/otel";
+import { SamplingDecision } from "@opentelemetry/sdk-trace-base";
+import { Context, trace } from "@opentelemetry/api";
 
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
@@ -8,6 +11,37 @@ export async function register() {
   if (process.env.NEXT_RUNTIME === "edge") {
     await import("../sentry.edge.config");
   }
+
+  registerOTel({
+    serviceName: "boudoir-barcelona-app",
+    traceExporter: "auto",
+    spanProcessors: ["auto"],
+    traceSampler: {
+      shouldSample: (context: Context) => {
+        const isChecklySpan = trace
+          .getSpan(context)
+          ?.spanContext()
+          ?.traceState?.get("checkly");
+        if (isChecklySpan) {
+          console.log(
+            "Sampling decision for Checkly span:",
+            SamplingDecision.RECORD_AND_SAMPLED,
+          );
+          return {
+            decision: SamplingDecision.RECORD_AND_SAMPLED,
+          };
+        } else {
+          console.log(
+            "Sampling decision for non-Checkly span:",
+            SamplingDecision.NOT_RECORD,
+          );
+          return {
+            decision: SamplingDecision.NOT_RECORD,
+          };
+        }
+      },
+    },
+  });
 }
 
 export const onRequestError = Sentry.captureRequestError;
