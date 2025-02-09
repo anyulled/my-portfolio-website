9dbf8107a7b65ad5d346eb0536d9d0c4c67d637b
Anyul Rivas <anyul.rivas@ingrammicro.com>
2025-02-09 16:54:00 +0100

    feat: remove openTelemetry temporarily

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,7 +1,4 @@
 import * as Sentry from "@sentry/nextjs";
-import { registerOTel } from "@vercel/otel";
-import { SamplingDecision } from "@opentelemetry/sdk-trace-base";
-import { Context, trace } from "@opentelemetry/api";
 
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
@@ -11,37 +8,6 @@ export async function register() {
   if (process.env.NEXT_RUNTIME === "edge") {
     await import("../sentry.edge.config");
   }
-
-  registerOTel({
-    serviceName: "boudoir-barcelona-app",
-    traceExporter: "auto",
-    spanProcessors: ["auto"],
-    traceSampler: {
-      shouldSample: (context: Context) => {
-        const isChecklySpan = trace
-          .getSpan(context)
-          ?.spanContext()
-          ?.traceState?.get("checkly");
-        if (isChecklySpan) {
-          console.log(
-            "Sampling decision for Checkly span:",
-            SamplingDecision.RECORD_AND_SAMPLED,
-          );
-          return {
-            decision: SamplingDecision.RECORD_AND_SAMPLED,
-          };
-        } else {
-          console.log(
-            "Sampling decision for non-Checkly span:",
-            SamplingDecision.NOT_RECORD,
-          );
-          return {
-            decision: SamplingDecision.NOT_RECORD,
-          };
-        }
-      },
-    },
-  });
 }
 
 export const onRequestError = Sentry.captureRequestError;
