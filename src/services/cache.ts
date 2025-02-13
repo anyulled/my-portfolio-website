15a69baceefef867565b1859972dbd0c9a356a0c
Anyul Rivas <anyul.rivas@ingrammicro.com>
2025-02-13 09:49:50 +0100

    feat: logging for caching mechanism

--- src/services/cache.ts
+++ src/services/cache.ts
@@ -1,8 +1,11 @@
 import { list, put } from "@vercel/blob";
 import { Photo } from "@/services/flickr";
+import chalk from "chalk";
 
 export async function getCachedData(key: string): Promise<Photo[] | null> {
   const response = await list();
+  console.log(chalk.green(`Cache Read Success (${key}):`));
+  console.log(chalk.cyan("Cache response"), response);
   const matchingBlob = response.blobs.find((b) => b.pathname === key);
 
   if (!matchingBlob) {
@@ -18,10 +21,11 @@ export async function setCachedData(
   expiryInSeconds: number,
 ): Promise<void> {
   const serializedData = JSON.stringify(data);
-  await put(key, serializedData, {
+  const result = await put(key, serializedData, {
     contentType: "application/json",
     access: "public",
     cacheControlMaxAge: expiryInSeconds,
   });
   console.log(`Cache Write Success (${key}):`);
+  console.log(chalk.cyan("Cache response"), result);
 }
