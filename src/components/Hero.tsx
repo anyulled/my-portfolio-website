05a702c6c0c50184755a1a487c4a52bb056d18aa
Anyul Rivas <anyul.rivas@ingrammicro.com>
2025-03-06 11:57:49 +0100

    style: mocha mousse adjustments.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Aref_Ruqaa, Dancing_Script } from "next/font/google";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useTranslations } from "next-intl";
 
 const arefRuqaa = Aref_Ruqaa({ subsets: ["latin"], weight: "400" });
@@ -8,29 +8,62 @@ const dancingScript = Dancing_Script({ subsets: ["latin"] });
 
 export default function Hero() {
   const t = useTranslations("home");
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
   const randomImage = React.useMemo(() => {
     const images = [
-      "https://live.staticflickr.com/65535/54349881217_a687110589_k_d.jpg",
-      "https://live.staticflickr.com/65535/53564630658_b2aeab68f2_h.jpg",
-      "https://live.staticflickr.com/65535/53367295647_2ff0fdf881_h.jpg",
-      "https://live.staticflickr.com/65535/53923428207_dc41871f93_h.jpg",
-      "https://live.staticflickr.com/65535/53564630648_06aa8a167d_h.jpg",
-      "https://live.staticflickr.com/65535/53963952034_7372534fc0_h.jpg",
+      {
+        image:
+          "https://live.staticflickr.com/65535/54349881217_a687110589_k_d.jpg",
+        position: "left top",
+      },
+      {
+        image:
+          "https://live.staticflickr.com/65535/53564630658_b2aeab68f2_h.jpg",
+        position: "left top",
+      },
+      {
+        image:
+          "https://live.staticflickr.com/65535/53367295647_2ff0fdf881_h.jpg",
+        position: "left top",
+      },
+      {
+        image:
+          "https://live.staticflickr.com/65535/53923428207_dc41871f93_h.jpg",
+        position: "left top",
+      },
+      {
+        image:
+          "https://live.staticflickr.com/65535/53564630648_06aa8a167d_h.jpg",
+        position: "center top",
+      },
+      {
+        image:
+          "https://live.staticflickr.com/65535/53963952034_7372534fc0_h.jpg",
+        position: "right top",
+      },
     ];
     return images[Math.floor(Math.random() * images.length)];
   }, []);
+
+  if (!mounted) {
+    return null;
+  }
 
   return (
     <section className="relative h-screen flex items-center justify-center overflow-hidden bg-mocha-mousse-50 dark:bg-mocha-mousse-900 text-mocha-mousse-900 dark:text-mocha-mousse-50">
       <div
         className="absolute inset-0 z-0"
         style={{
-          backgroundImage: `url('${randomImage}')`,
+          backgroundImage: `url('${randomImage.image}')`,
           backgroundAttachment: "fixed",
-          backgroundPosition: "center",
+          backgroundPosition: randomImage.position,
           backgroundSize: "cover",
-          mask: "linear-gradient(to bottom, black 66%, transparent 90%)",
+          mask: "linear-gradient(to bottom, white 66%, transparent 95%)",
         }}
       />
       <div className="relative z-10 text-center">
