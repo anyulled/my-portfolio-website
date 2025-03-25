0ec87d1bccd49791408c04b385140eadc1768665
Anyul Rivas <anyul.rivas@ingrammicro.com>
2025-03-25 19:44:08 +0100

    style: new color for cookie consent component

--- src/components/CookieConsent.tsx
+++ src/components/CookieConsent.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { Button } from "@/components/ui/button";
-import { grantConsent } from "@/lib/gtag";
-import { useTranslations } from "next-intl";
+import {useEffect, useState} from "react";
+import {Button} from "@/components/ui/button";
+import {grantConsent} from "@/lib/gtag";
+import {useTranslations} from "next-intl";
 
 export default function CookieConsent() {
   const [showConsent, setShowConsent] = useState(false);
@@ -26,17 +26,17 @@ export default function CookieConsent() {
 
   return (
     <div
-      className={`fixed bottom-0 left-0 right-0 p-2 dark:bg-gray-800 bg-gray-100 bg-opacity-75 backdrop-blur-sm z-50 text-sm transition-all duration-300 ${showConsent ? "translate-y-0" : "translate-y-full"}`}
+        className={`fixed bottom-0 left-0 right-0 p-2 dark:bg-mocha-mousse-800 bg-mocha-mousse-100 bg-opacity-75 backdrop-blur-sm z-50 text-sm transition-all duration-300 ${showConsent ? "translate-y-0" : "translate-y-full"}`}
     >
       <div className="container mx-auto flex justify-between items-center">
-        <p className="dark:text-gray-300 text-neutral-800">
+        <p className="dark:text-mocha-mousse-200 text-mocha-mousse-800">
           {t("cookie_message")}
         </p>
         <Button
           onClick={handleAccept}
           variant="outline"
           size="sm"
-          className="dark:hover:bg-gray-700 hover:bg-gray-200 dark:text-gray-300 text-neutral-800"
+          className="dark:hover:bg-mocha-mousse-700 dark:text-mocha-mousse-200 dark:border-mocha-mousse-700 hover:bg-mocha-mousse-200  text-mocha-mousse-800 border-mocha-mousse-200"
         >
           {t("accept")}
         </Button>
