f56986588b16f4917edabba4831750bf79e8af7c
Anyul Rivas <anyul.rivas@ingrammicro.com>
2025-05-21 15:23:55 +0200

    feat(SocialMedia): update social media icons to use Simple Icons and improve accessibility

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -1,14 +1,14 @@
 import Link from "next/link";
+import type { SimpleIcon } from "simple-icons";
 import {
-  Bird,
-  Camera,
-  Instagram,
-  LayoutPanelTop,
-  Twitter,
-  User
-} from "lucide-react";
+  siBluesky,
+  siFlickr,
+  siInstagram,
+  siKeras,
+  siThemodelsresource,
+  siX
+} from "simple-icons";
 import { Aref_Ruqaa } from "next/font/google";
-
 import {
   Tooltip,
   TooltipContent,
@@ -20,21 +20,31 @@ import FadeInTitle from "@/components/FadeInTitle";
 
 const arefRuqaa = Aref_Ruqaa({ subsets: ["latin"], weight: "400" });
 
-const socialLinks = [
-  { name: "Twitter", icon: Twitter, url: "https://twitter.com/anyulled" },
+interface SocialLinks {
+  name: string;
+  icon: SimpleIcon;
+  url: string;
+}
+
+const socialLinks: Array<SocialLinks> = [
+  { name: "Twitter", icon: siX, url: "https://twitter.com/anyulled" },
   {
     name: "Instagram",
-    icon: Instagram,
-    url: "https://instagram.com/sensuelleboudoir",
+    icon: siInstagram,
+    url: "https://instagram.com/sensuelleboudoir"
   },
-  { name: "Flickr", icon: Camera, url: "https://flickr.com/people/anyulled" },
-  { name: "Kavyar", icon: LayoutPanelTop, url: "https://kavyar.com/anyulled" },
-  { name: "Model Mayhem", icon: User, url: "https://modelmayhem.com/anyulled" },
+  { name: "Flickr", icon: siFlickr, url: "https://flickr.com/people/anyulled" },
+  { name: "Kavyar", icon: siKeras, url: "https://kavyar.com/anyulled" },
+  {
+    name: "Model Mayhem",
+    icon: siThemodelsresource,
+    url: "https://modelmayhem.com/anyulled"
+  },
   {
     name: "Bluesky",
-    icon: Bird,
-    url: "https://bsky.app/profile/sensuelle-boudoir.bsky.social",
-  },
+    icon: siBluesky,
+    url: "https://bsky.app/profile/sensuelle-boudoir.bsky.social"
+  }
 ];
 
 export default function SocialMedia() {
@@ -43,11 +53,11 @@ export default function SocialMedia() {
     <section className="my-6 bg-opacity-75 backdrop-blur-md">
       <div className="container mx-auto px-6">
         <FadeInTitle>
-        <h2
-          className={`${arefRuqaa.className} text-3xl font-bold text-center mb-8`}
-        >
-          {t("connect")}
-        </h2>
+          <h2
+            className={`${arefRuqaa.className} text-3xl font-bold text-center mb-8`}
+          >
+            {t("connect")}
+          </h2>
         </FadeInTitle>
         <TooltipProvider>
           <div className="flex justify-center space-x-6">
@@ -60,8 +70,15 @@ export default function SocialMedia() {
                     target="_blank"
                     rel="noopener noreferrer"
                   >
-                    <social.icon />
-                    <span className="sr-only">{social.name}</span>
+                    <svg
+                      className="fill-mocha-mousse-900 dark:fill-mocha-mousse-100"
+                      xmlns="http://www.w3.org/2000/svg" width={24} height={24}
+                      viewBox="0 0 24 24">
+                      <title>{social.name}</title>
+                      <path d={social.icon.path} />
+                    </svg>
+                    <span
+                      className="sr-only">{social.name}</span>
                   </Link>
                 </TooltipTrigger>
                 <TooltipContent>
