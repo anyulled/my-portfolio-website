ba1728ad76befb73125291a3c608db2cade5ecc9
Anyul Rivas <anyulled@gmail.com>
2025-06-30 14:29:31 +0200

    feat: image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    unoptimized: true,
+    unoptimized: false,
     remotePatterns: [
       {
         protocol: "https",
