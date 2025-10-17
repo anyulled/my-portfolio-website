4c1472530808ccdace539b4604c84239f894f139
Anyul Rivas <anyul.rivas@ingrammicro.com>
2025-10-17 14:39:56 +0200

    refactor(mixpanel): improve initialization logic and warn on missing token in non-production environments

--- src/lib/mixpanelClient.ts
+++ src/lib/mixpanelClient.ts
@@ -2,10 +2,12 @@ import mixpanel from "mixpanel-browser";
 
 const MIXPANEL_TOKEN = process.env.NEXT_PUBLIC_MIXPANEL_TOKEN;
 export const initMixpanel = () => {
-  if (!MIXPANEL_TOKEN) {
+  if (MIXPANEL_TOKEN) {
+    mixpanel.init(MIXPANEL_TOKEN, {
+      autocapture: true,
+      debug: process.env.NODE_ENV !== "production"
+    });
+  } else if (process.env.NODE_ENV !== "production") {
     console.warn("Mixpanel token is missing! Check your .env file.");
-    return;
   }
-
-  mixpanel.init(MIXPANEL_TOKEN, { autocapture: true });
 };
