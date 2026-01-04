5ceb0319fdbf08775a432ad7ae1c892deaa24c8f
Anyul Rivas <anyulled@gmail.com>
2026-01-04 23:48:00 +0100

    ci: Add GitHub Actions workflow and configure Nx to track the CI workflow file as a named input.

--- debug_eslint.mjs
+++
@@ -1,5 +0,0 @@
-import tseslint from 'typescript-eslint';
-console.log('tseslint keys:', Object.keys(tseslint));
-console.log('tseslint.config:', typeof tseslint.config);
-console.log('tseslint.defineConfig:', typeof tseslint.config);
-
